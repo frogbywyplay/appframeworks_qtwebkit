frogbywyplay/appframeworks_qtwebkit

commit a45c892ec9dc6b63f5106ced40a4fb7d168c7aad

Ensure replaced tiles are repainted
If a layer with a backing store is resized, we may replace some tiles
that were not at maximum size. When that happens we need to ensure the
replaced area is repainted, otherwise the content disappears until
refreshed.

Change-Id: Ie2f7796a2493a56ed5fc6cc4cea55f4ab9f38bbe
Reviewed-by: Michael Bruning <michael.bruning@digia.com>

Source/WebCore/platform/graphics/texmap/TextureMapperBackingStore.cpp
@@ -145,7 +145,7 @@ void TextureMapperTiledBackingStore::paintToTextureMapper(TextureMapper* texture
     }
 }
 
-void TextureMapperTiledBackingStore::createOrDestroyTilesIfNeeded(const FloatSize& size, const IntSize& tileSize, bool hasAlpha)
+void TextureMapperTiledBackingStore::createOrDestroyTilesIfNeeded(const FloatSize& size, const IntSize& tileSize, bool hasAlpha, IntRect& repaintRect)
 {
     if (size == m_size)
         return;
@@ -183,8 +183,12 @@ void TextureMapperTiledBackingStore::createOrDestroyTilesIfNeeded(const FloatSiz
         }
 
         // This tile is not needed.
-        if (!existsAlready)
+        if (!existsAlready) {
             tileIndicesToRemove.append(i);
+            // If the removed tile was painted ensure the area it covered is repainted.
+            if (m_tiles[i].texture())
+                repaintRect.unite(enclosingIntRect(oldTile));
+        }
     }
 
     // Recycle removable tiles to be used for newly requested tiles.
@@ -211,16 +215,18 @@ void TextureMapperTiledBackingStore::createOrDestroyTilesIfNeeded(const FloatSiz
 
 void TextureMapperTiledBackingStore::updateContents(TextureMapper* textureMapper, Image* image, const FloatSize& totalSize, const IntRect& dirtyRect, BitmapTexture::UpdateContentsFlag updateContentsFlag)
 {
-    createOrDestroyTilesIfNeeded(totalSize, textureMapper->maxTextureSize(), image->currentFrameHasAlpha());
+    IntRect repaintRect(dirtyRect);
+    createOrDestroyTilesIfNeeded(totalSize, textureMapper->maxTextureSize(), image->currentFrameHasAlpha(), repaintRect);
     for (size_t i = 0; i < m_tiles.size(); ++i)
-        m_tiles[i].updateContents(textureMapper, image, dirtyRect, updateContentsFlag);
+        m_tiles[i].updateContents(textureMapper, image, repaintRect, updateContentsFlag);
 }
 
 void TextureMapperTiledBackingStore::updateContents(TextureMapper* textureMapper, GraphicsLayer* sourceLayer, const FloatSize& totalSize, const IntRect& dirtyRect, BitmapTexture::UpdateContentsFlag updateContentsFlag)
 {
-    createOrDestroyTilesIfNeeded(totalSize, textureMapper->maxTextureSize(), true);
+    IntRect repaintRect(dirtyRect);
+    createOrDestroyTilesIfNeeded(totalSize, textureMapper->maxTextureSize(), true, repaintRect);
     for (size_t i = 0; i < m_tiles.size(); ++i)
-        m_tiles[i].updateContents(textureMapper, sourceLayer, dirtyRect, updateContentsFlag);
+        m_tiles[i].updateContents(textureMapper, sourceLayer, repaintRect, updateContentsFlag);
 }
 
 PassRefPtr<BitmapTexture> TextureMapperTiledBackingStore::texture() const

Source/WebCore/platform/graphics/texmap/TextureMapperBackingStore.h
@@ -103,7 +103,7 @@ public:
 
 private:
     TextureMapperTiledBackingStore();
-    void createOrDestroyTilesIfNeeded(const FloatSize& backingStoreSize, const IntSize& tileSize, bool hasAlpha);
+    void createOrDestroyTilesIfNeeded(const FloatSize& backingStoreSize, const IntSize& tileSize, bool hasAlpha, IntRect& repaintRect);
     void updateContentsFromImageIfNeeded(TextureMapper*);
     inline FloatRect rect() const { return FloatRect(FloatPoint::zero(), m_size); }
 

commit bb547c610b526adfa530bb621814a25962808150

Fix createOrDestroyTilesIfNeeded repaint rect extension algorithm
Fix a regression induced with commit a45c892ec9dc6b63f5106ced40a4fb7d168c7aad. This
commit is basically ensuring that part of the screen that contains layer that are
removed are repainted. Unfortunatly the createOrDestroyTilesIfNeeded was to eager to
expand this rect, and in some cases it became bigger than the size of the backing
store itself, resulting in memory corruption.

Source/WebCore/platform/graphics/texmap/TextureMapperBackingStore.cpp
@@ -185,9 +185,15 @@ void TextureMapperTiledBackingStore::createOrDestroyTilesIfNeeded(const FloatSiz
         // This tile is not needed.
         if (!existsAlready) {
             tileIndicesToRemove.append(i);
-            // If the removed tile was painted ensure the area it covered is repainted.
-            if (m_tiles[i].texture())
-                repaintRect.unite(enclosingIntRect(oldTile));
+            // If the removed tile was painted ensure the area it covered is repainted. However the
+            // resulting repaint rect must never cross the rect() boundaries as other functions
+            // using it wont check if it expands it too much, leading to memory corruption during
+            // copy operations.
+            if (m_tiles[i].texture()) {
+                FloatRect oldTileX(oldTile);
+                oldTileX.intersect(rect());
+                repaintRect.unite(enclosingIntRect(oldTileX));
+            }
         }
     }
 